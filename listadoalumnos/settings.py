6c52d84d751e619029a7bfbb17534e5050a95a8c
blocklal <lagoslautaro7@gmail.com>
2025-11-26 17:17:05 -0300

    aaa

--- listadoalumnos/settings.py
+++ listadoalumnos/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@j0q^g-xvy&)6fy$0l@(&s)4_40kov3@2r-)w%3w0trlzpp=jr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = os.environ.get('DEBUG', 'False') == 'False'
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
